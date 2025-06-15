ee37283a872a7656f6832460f3f0d39140c52e73
Queue-Evgenii <genadolgii228@gmail.com>
2025-06-15 13:41:35 +0300

    test

--- backend/index.php
+++ backend/index.php
@@ -1,16 +1,36 @@
 <?php
 $nestUrl = 'http://127.0.0.1:3000';
 
-// Убираем "/price-api.polandgroups.pl" из пути
 $path = $_SERVER['REQUEST_URI'];
-$cleanPath = preg_replace('#^/pl-price-api-admin/backend\.polandgroups\.pl#', '', $path);
+$cleanPath = preg_replace('#^/pl-price-api-admin#', '', $path);
 
-// Проксируем запрос
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $nestUrl . $cleanPath);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, false);
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
+
+if (in_array($_SERVER['REQUEST_METHOD'], ['POST', 'PUT', 'PATCH'])) {
+    curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents('php://input'));
+}
+
+$headers = [];
+foreach (getallheaders() as $name => $value) {
+    if (strtolower($name) !== 'host') {
+        $headers[] = "$name: $value";
+    }
+}
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
 $response = curl_exec($ch);
+
+if(curl_errno($ch)) {
+    http_response_code(500);
+    echo 'Curl error: ' . curl_error($ch);
+    curl_close($ch);
+    exit;
+}
 
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
