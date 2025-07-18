71539f05f7cb52aa07ffd0c90a0df2a4e2bf11bb
Queue-Evgenii <genadolgii228@gmail.com>
2025-07-18 19:13:47 +0300

    extend mime types in proxy

--- index.php
+++ index.php
@@ -169,7 +169,25 @@ function proxyRequest(string $url)
 
 function serveStaticFile(string $filePath)
 {
-    $mimeType = mime_content_type($filePath) ?: 'application/octet-stream';
+    $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+    
+    $mimeTypes = [
+        'js'   => 'application/javascript',
+        'css'  => 'text/css',
+        'html' => 'text/html',
+        'json' => 'application/json',
+        'png'  => 'image/png',
+        'jpg'  => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'svg'  => 'image/svg+xml',
+        'woff' => 'font/woff',
+        'woff2'=> 'font/woff2',
+        'ttf'  => 'font/ttf',
+        'eot'  => 'application/vnd.ms-fontobject',
+        'otf'  => 'font/otf',
+    ];
+
+    $mimeType = $mimeTypes[$extension] ?? mime_content_type($filePath) ?? 'application/octet-stream';
     header('Content-Type: ' . $mimeType);
     header('Content-Length: ' . filesize($filePath));
     readfile($filePath);
