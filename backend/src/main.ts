0120cd73120c0f9da90d60cfb30963b07fca5849
Queue-Evgenii <genadolgii228@gmail.com>
2025-08-20 19:26:02 +0300

    ADD: password in swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
 
 void (async () => {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,14 @@ void (async () => {
   app.enableCors();
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.use(
+    ['/swagger'],
+    basicAuth({
+      users: { [process.env.SWAGGER_USER as string]: process.env.SWAGGER_PASS as string },
+      challenge: true,
+    }),
+  );
 
   const config = new DocumentBuilder().setTitle('PL PRICE API').setVersion('1.0').build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
