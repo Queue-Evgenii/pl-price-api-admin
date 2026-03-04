541235265da31ca10354d9e6d0ffdede36e0d2f8
sundevildi <sundevildi@gmail.com>
2026-03-04 13:30:33 +0100

    Use JavaScript to clear hash before redirect

--- redirect.php
+++ redirect.php
@@ -1,4 +1,19 @@
 <?php
-header("Location: https://polandgroup.pl/", true, 301);
-exit();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting...</title>
+    <script>
+        // Clear the hash first, then redirect
+        window.location.hash = '';
+        setTimeout(function() {
+            window.location.href = 'https://polandgroup.pl/#/pl/categories';
+        }, 100);
+    </script>
+</head>
+<body>
+    <p>Redirecting...</p>
+</body>
+</html>
